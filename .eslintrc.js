566fcd1ed1bc3d66cbdd0e8589a8c92cb41f2ab7
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-02-03 18:33:48 +0530

    setup of eslint in project

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "next",
+        "airbnb",
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      "react/jsx-filename-extension": [1, { "extensions": [".jsx",".tsx"] }],
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/quotes": "off",
+    }
+}
