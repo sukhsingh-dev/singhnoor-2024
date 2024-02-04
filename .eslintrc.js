923ba08c3cc3dce90cf9b680f7b15453931245d8
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-02-04 16:10:50 +0530

    added images in public folder

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,41 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "next",
-        "airbnb",
-        "standard-with-typescript",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-      "react/jsx-filename-extension": [1, { "extensions": [".jsx",".tsx"] }],
-      "react/react-in-jsx-scope": "off",
-      "@typescript-eslint/quotes": "off",
-      "@typescript-eslint/space-before-function-paren": "off",
-      "react/function-component-definition": "off",
-      "import/extensions": "off"
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "next",
+    "airbnb",
+    "standard-with-typescript",
+    "plugin:react/recommended"
+  ],
+  "overrides": [
+    {
+      "env": {
+        "node": true
+      },
+      "files": [
+        ".eslintrc.{js,cjs}"
+      ],
+      "parserOptions": {
+        "sourceType": "script"
+      }
     }
-}
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "react/jsx-filename-extension": [1, { "extensions": [".jsx", ".tsx"] }],
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/quotes": "off",
+    "@typescript-eslint/space-before-function-paren": "off",
+    "react/function-component-definition": "off",
+    "import/extensions": "off",
+    "linebreak-style": "off"
+  }
+};
