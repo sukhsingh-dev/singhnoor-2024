9619a6741d3aa3d25e9945261982412829f064e6
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-02-04 14:35:46 +0530

    added styling setup for project

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import "./styles.module.sass"
+
+export default function Test(): React.ReactNode {
+  return (
+    <>
+      <h1>test page</h1>
+      <button type="button" className="btn btn-primary">Primary button</button>
+    </>
+  )
+}
