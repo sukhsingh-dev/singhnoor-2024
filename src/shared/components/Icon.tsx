e51f51d055e1111e9c9cef23cd43d7c58db03e3b
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-02-05 14:51:48 +0530

    Removed deprecated default props from Icon component

--- src/shared/components/Icon.tsx
+++ src/shared/components/Icon.tsx
@@ -5,16 +5,10 @@ interface IconType {
   height?: number
 }
 
-const Icon: React.FC<IconType> = ({ width, height, name, className }: IconType) => (
+const Icon: React.FC<IconType> = ({ width = 24, height = 24, name, className = '' }: IconType) => (
   <svg width={width} height={height} className={`icon ${className}`}>
     <use href={`/icons/sprite.svg#${name}`} />
   </svg>
 )
-
-Icon.defaultProps = {
-  width: 24,
-  height: 24,
-  className: ''
-}
 
 export default Icon
