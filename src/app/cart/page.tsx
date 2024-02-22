c505f8af81149c4b3b1fc5f31acab5b0d1e120ae
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-02-22 16:21:00 +0530

    added cart and wishlist pages body

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,5 @@
+const CartPage = (): React.ReactNode => (
+  <h1>Cart Page</h1>
+)
+
+export default CartPage
---
+++ src/app/wishlist/page.tsx
@@ -0,0 +1,5 @@
+const WishListPage = (): React.ReactNode => (
+  <h1>Wishlist Page</h1>
+)
+
+export default WishListPage
