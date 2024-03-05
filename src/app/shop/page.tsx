360bb7ca25ca127c9eec99aa59b1a7eb4d5fa896
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-03-05 16:59:43 +0530

    added filter option on shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -9,13 +9,13 @@ const ShopPage = (): React.ReactNode => {
     <section className="shop-page-section section-more-width">
       <div className="filters-fields-outer">
         <div className="shop-page-actions view-group">
-          <button
-            type="button"
+          <label
+            htmlFor="filter-fields-toggler"
             className="btn btn-primary btn-filter"
           >
             Filter
             <Icon name="filter" />
-          </button>
+          </label>
 
           <fieldset className="shop-page-actions-view">
             <input type="radio" name="productView" id="productViewGrid" value="productViewGrid" defaultChecked />
@@ -28,9 +28,16 @@ const ShopPage = (): React.ReactNode => {
             </label>
           </fieldset>
         </div>
+        <input type="checkbox" className="filter-fields-toggler" id="filter-fields-toggler" />
         <div className="filters-fields">
           <div className="filters-fields-heading">
             <h2>Filters</h2>
+            <label
+              className="filter-fields-toggler-close"
+              htmlFor="filter-fields-toggler"
+            >
+              <Icon name="close" />
+            </label>
             <button type="button" className="btn-clear">
               Clear all
               <Icon name="close" width={16} height={16} />
@@ -38,13 +45,160 @@ const ShopPage = (): React.ReactNode => {
           </div>
           <div className="filters-fields-body">
             <ul className="filters-fields-main">
-              <li>Category</li>
-              <li>For</li>
-              <li>Material</li>
-              <li>Sizes</li>
-              <li>Colors</li>
-              <li>Price Rang</li>
+              <li>
+                <details className="filters-fields-toggler">
+                  <summary>Category</summary>
+                  <p className="filters-fields-options">
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">Gatra</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">Belts</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">T-shirts</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">Bags</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                  </p>
+                </details>
+              </li>
+              <li>
+                <details className="filters-fields-toggler">
+                  <summary>For</summary>
+                  <p className="filters-fields-options">
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">Men</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">Women</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">Kids</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">Couple</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                  </p>
+                </details>
+              </li>
+              <li>
+                <details className="filters-fields-toggler">
+                  <summary>Material</summary>
+                  <p className="filters-fields-options">
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">Cotton</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">Leather</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">Resham</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                  </p>
+                </details>
+              </li>
+              <li>
+                <details className="filters-fields-toggler">
+                  <summary>Sizes</summary>
+                  <p className="filters-fields-options">
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">XS</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">M</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">L</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">XL</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                    <label className="sn-custom-checkbox-outer">
+                      <span className="sn-custom-checkbox-label">XXL</span>
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" />
+                    </label>
+                  </p>
+                </details>
+              </li>
+              <li>
+                <details className="filters-fields-toggler">
+                  <summary>Colors</summary>
+                  <p className="filters-fields-options color">
+                    <label className="sn-custom-checkbox-outer color">
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" style={{ backgroundColor: '#000' }} />
+                    </label>
+
+                    <label className="sn-custom-checkbox-outer color">
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" style={{ backgroundColor: 'red' }} />
+                    </label>
+                    <label className="sn-custom-checkbox-outer color">
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox" style={{ backgroundColor: 'green' }} />
+                    </label>
+                    <label className="sn-custom-checkbox-outer color">
+                      <input type="checkbox" className="sn-custom-checkbox-input" />
+                      <span className="sn-custom-checkbox white" style={{ backgroundColor: 'white' }} />
+                    </label>
+                  </p>
+                </details>
+              </li>
+              <li>
+                <details className="filters-fields-toggler">
+                  <summary>Price Range</summary>
+                  <p className="filters-fields-options">
+                    <input type="range" className="sn-input-range" />
+                    <span className="sn-input-range-set d-flex justify-between">
+                      <label>
+                        <span>Min</span>
+                        <input defaultValue="399" type="number" />
+                      </label>
+                      <label>
+                        <span>Max</span>
+                        <input defaultValue="9999" type="number" />
+                      </label>
+                    </span>
+                  </p>
+                </details>
+              </li>
             </ul>
+          </div>
+          <div className="filters-fields-footer">
+            <button type="button" className="btn btn-primary">Apply Filters</button>
           </div>
         </div>
       </div>
