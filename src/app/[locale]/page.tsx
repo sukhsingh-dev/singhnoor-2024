d543fe2b44076ce3d8e5d51462306798e7648dcd
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-03-06 17:41:45 +0530

    added translation for few pages

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import { productList, productList2 } from "@/shared/helper/store"
+import { useTranslations } from "next-intl"
 import Slider from "./home/slider"
 import Categories from "./home/categories"
 import Products from "./home/products"
@@ -9,14 +10,16 @@ import GetTheLook from "./home/looks"
 import KhalsaCollections from "./home/khalsa"
 
 export default function Home(): React.ReactNode {
+  const t = useTranslations("Index")
+
   return (
     <>
       <Slider />
       <Categories />
-      <Products heading="TOP PICKS" productList={productList2} moreLink="/shop" outerClass="section-more-width top-picks" />
+      <Products heading={t('topPicks')} productList={productList2} moreLink="/shop" outerClass="section-more-width top-picks" />
       <Collections />
       <TodayDeal />
-      <Products heading="MOST POPULAR" productList={productList} moreLink="/shop" outerClass="section-more-width more-popular" />
+      <Products heading={t('mostPopular')} productList={productList} moreLink="/shop" outerClass="section-more-width more-popular" />
       <GetTheLook />
       <KhalsaCollections />
       <DealsZone />
