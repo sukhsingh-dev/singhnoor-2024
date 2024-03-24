27414dea77f4636d3a2dea9d0f330288bf5c6770
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-03-24 19:46:32 +0530

    minor link updates on categories

--- src/app/[locale]/home/categories/index.tsx
+++ src/app/[locale]/home/categories/index.tsx
@@ -1,5 +1,5 @@
 import { Heading, TextDecorator } from "@/shared/components/ui"
-import { useTranslations } from "next-intl"
+import { useLocale, useTranslations } from "next-intl"
 import Link from "next/link"
 import Image from "next/image"
 import './categories.sass'
@@ -13,6 +13,7 @@ interface CategoryType {
 
 const Categories: React.FC = async () => {
   const t = useTranslations("Index")
+  const lang = useLocale()
   const res = await fetch(`${process.env.BACKOFFICE_URL}/categories`)
   const categories = await res.json()
 
@@ -22,19 +23,21 @@ const Categories: React.FC = async () => {
       <ul className="categories-list">
         {
           categories.map((category: CategoryType) => (
-            <li className="text-center aos" key={category._id}>
-              <div className="bg-decorator mx-auto" style={{ backgroundColor: category.categoryBg }} />
-              <Image
-                src={category.categoryImg}
-                alt={category.categoryName}
-                width={171}
-                height={171}
-                className="object-contain"
-                quality={100}
-              />
-              <Link href={`/shop?category=${category._id}`} className="cat-link">
-                {category.categoryName}
-                <TextDecorator color={category.categoryBg} />
+            <li className="aos" key={category._id}>
+              <Link href={`${lang}/shop?category=${category._id}`} className="category-item-link">
+                <div className="bg-decorator mx-auto" style={{ backgroundColor: category.categoryBg }} />
+                <Image
+                  src={category.categoryImg}
+                  alt={category.categoryName}
+                  width={171}
+                  height={171}
+                  className="object-contain"
+                  quality={100}
+                />
+                <div className="cat-link">
+                  {category.categoryName}
+                  <TextDecorator color={category.categoryBg} />
+                </div>
               </Link>
             </li>
           ))
