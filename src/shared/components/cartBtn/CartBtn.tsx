76909f99e0734e4ae19b9366c84997f03ef8ec18
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-04-13 19:44:39 +0530

    added cart items in local storage

--- src/shared/components/cartBtn/CartBtn.tsx
+++ src/shared/components/cartBtn/CartBtn.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Icon from "../Icon"
 import Modal from "../ui/modal/Modal"
 
@@ -10,11 +10,40 @@ interface CartBtnTypes {
 }
 
 const CartBtn = ({ id, qty = 1 }: CartBtnTypes): React.ReactNode => {
+  const ls = typeof window !== "undefined" ? window.localStorage : null
   const [showAlert, setShowAlert] = useState(false)
+  const [cartProducts, setCartProducts] = useState<string[]>([])
 
   const addToCart = (itemId: string): void => {
     setShowAlert(true)
+    const cartDataString = ls?.getItem('sn-cart')
+    const oldCart = (cartDataString != null) ? JSON.parse(cartDataString) : null
+
+    if (oldCart === null) {
+      setCartProducts([itemId])
+    } else if (Array.isArray(oldCart)) {
+      setCartProducts([...oldCart, itemId])
+    } else {
+      setCartProducts([oldCart, itemId])
+    }
   }
+
+  useEffect(() => {
+    if (cartProducts?.length > 0) {
+      ls?.setItem('sn-cart', JSON.stringify(cartProducts))
+    }
+  }, [cartProducts, ls])
+
+  useEffect(() => {
+    const cartDataString = ls?.getItem('sn-cart')
+    if ((ls?.getItem('sn-cart')) !== null) {
+      const getCartData = (cartDataString != null) ? JSON.parse(cartDataString) : null
+      if (getCartData !== null) {
+        setCartProducts([getCartData])
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <>
