40d7771c5893073e2621b14fc40e40724fe931c4
sukhsingh-dev <sukhjindersinghmca@outlook.com>
2024-08-18 13:37:03 +0530

    fixed error of value on product page

--- src/app/product/client/ProductAttributes.tsx
+++ src/app/product/client/ProductAttributes.tsx
@@ -8,8 +8,9 @@ import { CART_STORE_NAME, WISHLIST_STORE_NAME } from "@/shared/helper/constants"
 import SizeChart from "./SizeChart"
 
 const ProductAttributes = ({ product }: Product): React.ReactNode => {
-  const [selectedSize, setSelectedSize] = useState<string | undefined>(product.productSize[0].value)
-  const [selectedColor, setSelectedColor] = useState<string | null>(product.productColors[0].value)
+  // eslint-disable-next-line max-len
+  const [selectedSize, setSelectedSize] = useState<string | undefined>(product.productSize[0]?.value)
+  const [selectedColor, setSelectedColor] = useState<string | null>(product.productColors[0]?.value)
   const [selectedQty, setSelectedQty] = useState<number>(1)
 
   const roundToNearestTen = (number: number): number => Math.round(number / 10) * 10
